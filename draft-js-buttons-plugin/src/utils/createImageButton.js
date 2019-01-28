ffa0f3fb82c9fd7c4dc2ec4f4786647591af667e
Alexey Pustobaev <L@whisperlab.com>
2019-01-29 00:27:53 +0300

    buttons-1.3.1

--- draft-js-buttons-plugin/src/utils/createImageButton.js
+++ draft-js-buttons-plugin/src/utils/createImageButton.js
@@ -20,6 +20,7 @@ export default ({ children }) => (
         const file = e.target.files[0]
         src = window.URL.createObjectURL(file)
       }
+      e.target.value = null
       if (!src) { return }
       const imageData = {src: src, type: 'placeholder'}
       this.props.setEditorState(insertDataBlock(this.props.getEditorState(), imageData, 'image'))
